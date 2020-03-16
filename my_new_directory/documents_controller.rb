e8970f4c1d73b2c46a1743a8fdacfba101a539b9
James Lenahan <james.k.lenahan@gmail.com>
2020-03-16 17:47:19 +0000

    Automatically backed up by Learn

--- my_new_directory/documents_controller.rb
+++ my_new_directory/documents_controller.rb
@@ -1,5 +1,21 @@
-class DocumentsController
+class DocumentsController < ApplicationController
   def show
+    return head(:forbidden) unless session.include? :user_id
     @document = Document.find(params[:id])
+  end
+ 
+  def index
+    return head(:forbidden) unless session.include? :user_id
+  end
+ 
+  def create
+    return head(:forbidden) unless session.include? :user_id
+    @document = Document.create(author_id: user_id)
+  end
+ 
+  def update
+    return head(:forbidden) unless session.include? :user_id
+    @document = Document.find(params[:id])
+    # code to update a document
   end
 end
